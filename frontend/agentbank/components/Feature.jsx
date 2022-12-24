a28263c70ba75df4bb1b6a692a1428bc0043fd74
mmi-dev <m.midoux@hotmail.fr>
2022-12-24 15:39:12 +0100

    fix: #1 landing page

---
+++ frontend/agentbank/components/Feature.jsx
@@ -0,0 +1,12 @@
+function Feature({title,icon,alt,text}) {
+    console.log({title,icon,alt,text})
+    return ( 
+        <div className="feature-item">
+            <img src={icon} alt={alt} className="feature-icon" />
+            <h3 className="feature-item-title">{title}</h3>
+            <p>{text}</p>
+          </div>
+     );
+}
+
+export default Feature;
---
+++ frontend/agentbank/services/Services.js
@@ -0,0 +1,6 @@
+import dataMocked from "../data/dataMocked.json"
+
+export async function getFeatures(){
+    const data = dataMocked.features
+    return data
+}
